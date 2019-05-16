de3cb9911375456b2c71f92bf2526498638a553e
ando07 <ando.masahito@gmail.com>
2019-05-17 08:56:43 +0900

    [modify]CORS setting.

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:8000"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"Authorization"})
+
 	r := mux.NewRouter()
 	r.HandleFunc("/public", public)
 	r.HandleFunc("/private", private)
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
 }
 
 func public(w http.ResponseWriter, r *http.Request) {
